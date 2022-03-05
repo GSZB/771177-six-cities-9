19e327bf408296d336550cab40b475408cb6e3dd
GSZB <szaboochka@gmail.com>
2022-03-05 22:29:53 +0100

    added the private route

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,10 +1,11 @@
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
-import { AppRoute } from '../../const';
+import { AppRoute, AuthorizationStatus } from '../../const';
 import MainPage from '../main-page/main-page';
 import SignIn from '../sign-in-screen/sign-in-screen';
 import Favorites from '../favorites-screen/favorites-screen';
 import Room from '../room-screen/room-screen';
 import NotFoundScreen from '../not-found-screen/not-found-screen';
+import PrivateRoute from '../private-route/private-route';
 
 
 type AppProps = {
@@ -15,26 +16,11 @@ function App({offersCount}: AppProps): JSX.Element {
   return(
     <BrowserRouter>
       <Routes>
-        <Route
-          path={AppRoute.Main}
-          element={<MainPage offersCount={offersCount} />}
-        />
-        <Route
-          path={AppRoute.SignIn}
-          element={<SignIn/>}
-        />
-        <Route
-          path={AppRoute.Favorites}
-          element={<Favorites/>}
-        />
-        <Route
-          path={AppRoute.Room}
-          element={<Room/>}
-        />
-        <Route
-          path='*'
-          element={<NotFoundScreen/>}
-        />
+        <Route index element={<MainPage offersCount={offersCount} />}/>
+        <Route path={AppRoute.SignIn} element={<SignIn/>}/>
+        <Route path={AppRoute.Favorites} element={<PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}><Favorites/></PrivateRoute>}/>
+        <Route path={AppRoute.Room} element={<Room/>}/>
+        <Route path='*' element={<NotFoundScreen/>}/>
       </Routes>
     </BrowserRouter>
   );
